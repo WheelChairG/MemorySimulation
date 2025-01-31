26a347cf4f51e8cb3cf9d2ae09a8018285e72740
yuchen <go98pif@mytum.de>
2025-01-31 19:25:20 +0100

    cacheline modified

--- src/cacheline.hpp
+++ src/cacheline.hpp
@@ -8,9 +8,10 @@ class CacheLine : sc_module{
     uint32_t tag;
     std::vector<std::vector<uint8_t>> data;
 
-    CacheLine(bool valid, uint32_t tag){
+    CacheLine(uint32_t line_size){
         valid = false;
-        
+        tag = 0;
+        data.resize(line_size);
     };
 
     uint32_t read(uint32_t tag);
